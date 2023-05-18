10a917f9ae25428d1bff8fbc4b0fe1048874f025
Petya Naydenova <122305964+Petya2023@users.noreply.github.com>
2023-05-19 00:00:01 +0300

    Create FootballPlayer.cs

---
+++ FootballPlayer.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Football
+{
+    class FootballPlayer
+    {
+        public int Number { get; set; }
+        public double Height { get; set; }
+    }
+}
